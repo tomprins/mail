2932cccd42b54cf997b5263400507e077e2232d6
Tom <tomprins64@gmail.com>
2026-02-28 11:25:19 +0100

    click mail in maillist shows mail in detail view + added formatting

--- src/main.js
+++ src/main.js
@@ -2,56 +2,68 @@
 // Be sure to set `app.withGlobalTauri` in `tauri.conf.json` to true
 const invoke = window.__TAURI__.core.invoke;
 
-const searchbarMail = document.getElementById("mail-searchbar").addEventListener("input", searchMail);
+document.addEventListener("DOMContentLoaded", initialize);
 
-function searchMail(event) {
-    console.log(`searching mails for '${event.target.value}'`)
-    setMailList(event.target.value)
+function initialize() {
+  document
+    .getElementById("mail-searchbar")
+    .addEventListener("input", searchMail);
 }
 
-async function setMailList(query = "*") {
-    let mails = await invoke('get_mails', { query: query });
+function searchMail(event) {
+  console.log(`searching mails for '${event.target.value}'`);
+  setMaillist(event.target.value);
+}
 
-    const mailList = document.getElementById("mail-list")
-    mailList.replaceChildren(); // Remove all previously added mails.
+async function setMaillist(query = "*") {
+  let mails = await invoke("get_mails", { query: query });
 
-    let tempMail;
-    for (let index = 0; index < mails.length; index++) {
-        const mail = mails[index];
-        const card = createMaillistEntry(mail.document.from, mail.document.subject)
-        mailList.appendChild(card)
+  const mailList = document.getElementById("maillist");
+  mailList.replaceChildren(); // Remove all previously added mails.
 
-        tempMail = mail;
-    }
+  for (let index = 0; index < mails.length; index++) {
+    const mail = mails[index];
 
-    const detailView = document.getElementById("detail-view")
-    detailView.replaceChildren()
+    const maillistEntry = createMaillistEntry(
+      mail.document.from,
+      mail.document.subject,
+    );
+    maillistEntry.addEventListener("mousedown", (event) => {
+      maillistMailClick(event, mail);
+    });
 
-    const mailBody = DOMPurify.sanitize(tempMail.document.raw_body);
-    detailView.innerHTML = mailBody
+    mailList.appendChild(maillistEntry);
+  }
+}
+
+function maillistMailClick(event, mail) {
+  const detailView = document.getElementById("detail-view");
+  detailView.replaceChildren();
+
+  const mailBody = DOMPurify.sanitize(mail.document.raw_body);
+  detailView.innerHTML = mailBody;
 }
 
 function createMaillistEntry(from, subject) {
-    const cardDiv = document.createElement("div")
-    cardDiv.className = "card"
+  const cardDiv = document.createElement("div");
+  cardDiv.className = "maillist-mail card";
 
-    const cardBody = document.createElement("div")
-    cardBody.className = "card-body"
+  const cardBody = document.createElement("div");
+  cardBody.className = "card-body";
 
-    const cardTitle = document.createElement("h5")
-    cardTitle.className = "card-title"
-    cardTitle.textContent = from
+  const cardTitle = document.createElement("h5");
+  cardTitle.className = "card-title";
+  cardTitle.textContent = from;
 
-    const cardText = document.createElement("p")
-    cardText.className = "card-text"
-    cardText.textContent = subject
+  const cardText = document.createElement("p");
+  cardText.className = "card-text";
+  cardText.textContent = subject;
 
-    cardBody.appendChild(cardTitle)
-    cardBody.appendChild(cardText)
-    cardDiv.appendChild(cardBody)
+  cardBody.appendChild(cardTitle);
+  cardBody.appendChild(cardText);
+  cardDiv.appendChild(cardBody);
 
-    return cardDiv
+  return cardDiv;
 }
 
-
-setMailList()
+setMaillist();
