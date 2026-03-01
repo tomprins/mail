715c2a052742892c69aa55efc736fa8acd5da8c6
Tom <tomprins64@gmail.com>
2026-03-01 09:44:36 +0100

    added 'create mail' popup

--- src/main.js
+++ src/main.js
@@ -3,13 +3,51 @@
 const invoke = window.__TAURI__.core.invoke;
 
 document.addEventListener("DOMContentLoaded", initialize);
+const scrollbarWidth = getScrollbarWidth();
 
 function initialize() {
-  document.getElementById("mail-searchbar").addEventListener("click", function (event) {
-    document.getElementById("mail-searchbar-input").focus()
-  })
+  const newMailPopup = document.getElementById("new-mail-popup");
 
-  document.getElementById("mail-searchbar-input").addEventListener("input", searchMail);
+  document
+    .getElementById("mail-searchbar")
+    .addEventListener("click", function (event) {
+      document.getElementById("mail-searchbar-input").focus();
+    });
+
+  document
+    .getElementById("mail-searchbar-input")
+    .addEventListener("input", searchMail);
+
+  document
+    .getElementById("new-mail-button")
+    .addEventListener("mousedown", function (event) {
+      newMailPopup.classList.remove("hidden");
+    });
+
+  document
+    .getElementById("new-mail-popup-close-icon")
+    .addEventListener("mousedown", function (event) {
+      newMailPopup.classList.add("hidden");
+    });
+
+  // TODO to class list
+  document
+    .getElementById("new-mail-popup-add-cc")
+    .addEventListener("mousedown", function (event) {
+      document
+        .getElementById("new-mail-popup-cc-row")
+        .toggleAttribute("hidden");
+
+      document
+        .getElementById("new-mail-popup-bcc")
+        .setAttribute("hidden", "hidden");
+    });
+
+  document
+    .getElementById("new-mail-popup-add-bcc")
+    .addEventListener("mousedown", function (event) {
+      document.getElementById("new-mail-popup-bcc").toggleAttribute("hidden");
+    });
 }
 
 function searchMail(event) {
@@ -32,11 +70,24 @@ async function setMaillist(query = "*") {
 }
 
 function maillistMailClick(event, mail) {
-  const detailView = document.getElementById("detail-view");
+  const detailView = document.getElementById("detail-view-mail-body");
   detailView.replaceChildren();
 
   const mailBody = DOMPurify.sanitize(mail.document.raw_body);
   detailView.innerHTML = mailBody;
+
+  // reset scroll
+  document.getElementById("detail-view-mail-body").scroll(0, 0);
+
+  // update width of create mail popup when mail has a scrollbar.
+  const newMailPopup = document.getElementById("new-mail-popup");
+  var right = 20;
+
+  if (hasVerticalScrollbar(detailView)) {
+    right += scrollbarWidth;
+  }
+
+  newMailPopup.style.cssText = `right: ${right}px;`;
 }
 
 function createMaillistEntry(mail) {
@@ -62,17 +113,32 @@ function createMaillistEntry(mail) {
     maillistMailClick(event, mail);
   });
 
-  maillistMail.addEventListener("mouseenter", (event) => {
-    maillistMail.style.backgroundColor = "#303030"
-    maillistMail.style.borderColor = "#575757"
-  })
-
-  maillistMail.addEventListener("mouseleave", (event) => {
-    maillistMail.style.backgroundColor = "#262626"
-    maillistMail.style.borderColor = "#474747"
-  })
-
   return maillistMail;
+}
+
+function getScrollbarWidth() {
+  // Creating invisible container
+  const outer = document.createElement("div");
+  outer.style.visibility = "hidden";
+  outer.style.overflow = "scroll"; // forcing scrollbar to appear
+  outer.style.msOverflowStyle = "scrollbar"; // needed for WinJS apps
+  document.body.appendChild(outer);
+
+  // Creating inner element and placing it in the container
+  const inner = document.createElement("div");
+  outer.appendChild(inner);
+
+  // Calculating difference between container's full width and the child width
+  const scrollbarWidth = outer.offsetWidth - inner.offsetWidth;
+
+  // Removing temporary elements from the DOM
+  outer.parentNode.removeChild(outer);
+
+  return scrollbarWidth;
+}
+
+function hasVerticalScrollbar(element) {
+  return element.scrollHeight > element.clientHeight;
 }
 
 setMaillist();
